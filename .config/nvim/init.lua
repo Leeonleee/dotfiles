40c9b1183f6168c407005413955ca564a1232fa2
Leon Lee <leonlee20031219@gmail.com>
2025-06-09 21:23:07 +1000

    Made changes to tmux and nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -833,6 +833,17 @@ require("lazy").setup({
 				pyright = {},
 				ts_ls = {},
 				html = {},
+				emmet_ls = {
+					filetypes = {
+						"html",
+						"css",
+						"javascript",
+						"javascriptreact",
+						"typescriptreact",
+						"svelte",
+						"vue",
+					},
+				},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
